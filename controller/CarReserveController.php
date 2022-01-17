38becb1c7eae28b8cf9bda915676e77ebe4e9c58
mosadeghe <90137057+mosadeghe-afzali@users.noreply.github.com>
2022-01-18 00:02:57 +0330

    Update CarReserveController.php

--- controller/CarReserveController.php
+++ controller/CarReserveController.php
@@ -7,18 +7,17 @@ class CarReserveController{
     public function insertReserveData()
     {
         try {
-            if(!isset($_SESSION['username'])){
+            if(!isset($_SESSION['username'])) {
                 header('Location:/reservation/LoginController/loginPage/');
             }
-
             $startDateTime = $_POST['start_date'] . ' ' . $_POST['start_time'];
             $endDateTime = $_POST['end_date'] . ' ' . $_POST['end_time'];
             $validation = new ReserveCarValidation($startDateTime, $endDateTime);
             $validation->validationRequest();
             $conn = new CarReserve($startDateTime, $endDateTime, $_POST['id']);
             $conn->insertData();
+            
             include_once 'view/passenger.php';
-
         }catch (ReserveValidationException $e){
             echo $e->getReserveValidationErrors();
         }
